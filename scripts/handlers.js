94ad5165b154158d6d9ae99475d68b6e18d1dc11
ajs6f <ajs6f@a7c3d585-246a-4d0f-aa80-7a775a0b11e9>
2009-01-06 16:37:39 +0000

    Toggle stresses and hint popup.
    git-svn-id: https://subversion.lib.virginia.edu/repos/prosody@60 a7c3d585-246a-4d0f-aa80-7a775a0b11e9

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -17,84 +17,110 @@ function init() {
 	$$('span[real]').collect( function(node) {
 		node.stress = ""
 	});
-	
+
 	$("toggle-discrepancies").toggle();
-	
+
 }
 
 function switchstress(shadowspan) {
 	// called when a syllable's stress is changed
-	realsyllable = $( "prosody:real:" + shadowspan.id.substring(15) );
+	realsyllable = $("prosody:real:" + shadowspan.id.substring(15));
 	if (realsyllable.stress == "-" || realsyllable.stress == "") {
-		new Effect.Opacity($(shadowspan), { from: 0, to: 1, duration: 0.4 });
+		new Effect.Opacity($(shadowspan), {
+			from :0,
+			to :1,
+			duration :0.4
+		});
 		shadowspan.removeAllChildren();
 		shadowspan.appendChild(marker(realsyllable));
 		realsyllable.stress = '+';
 
 	} else if (realsyllable.stress == "+") {
-		new Effect.Opacity($(shadowspan), { from: 1, to: 0, duration: 0.4 });
-		setTimeout(function() {
+		new Effect.Opacity($(shadowspan), {
+			from :1,
+			to :0,
+			duration :0.4
+		});
+		setTimeout( function() {
 			shadowspan.removeAllChildren();
 			shadowspan.appendChild(slackmarker(realsyllable));
-			realsyllable.stress = '∪';}, 150);
-		new Effect.Opacity($(shadowspan), { from: 0, to: 1, duration: 0.4 });
-	}
-	else {
-		new Effect.Opacity($(shadowspan), { from: 1, to: 0, duration: 0.4 });
-		setTimeout(function() {
-		shadowspan.removeAllChildren();
-		shadowspan.appendChild(placeholder());
-		realsyllable.stress = '-';}, 150);
+			realsyllable.stress = '∪';
+		}, 150);
+		new Effect.Opacity($(shadowspan), {
+			from :0,
+			to :1,
+			duration :0.4
+		});
+	} else {
+		new Effect.Opacity($(shadowspan), {
+			from :1,
+			to :0,
+			duration :0.4
+		});
+		setTimeout( function() {
+			shadowspan.removeAllChildren();
+			shadowspan.appendChild(placeholder());
+			realsyllable.stress = '-';
+		}, 150);
 	}
 }
 
 function checkstress(linenumber) {
 	// called to submit an answer to the scansion-checking servlet, returns the
 	// answer
-
+	debug("Entering checkstress(" + linenumber + ")");
 	// first we assemble the answer from the "stress" members of the appropriate
 	// line
-	var answer = $("prosody:real:" + linenumber).select("span[real]").pluck(
-			"stress").collect(function(s){return s.replace(/∪/,'-')}).join('');
 	
+	var answer = $("prosody:real:" + linenumber).select("span[real]").pluck(
+			"stress").collect( function(s) {
+		return s.replace(/∪/, '-')
+	}).join('');
+
 	// now we check to see that this is a complete answer. if not, we alert and
 	// return
-	
-	if ( answer.length != $("prosody:real:" + linenumber).select("span[real]").length ) {
+
+	if (answer.length != $("prosody:real:" + linenumber).select("span[real]").length) {
 		alert("An answer must be complete to be submitted. Please fill in a symbol over each syllable in this line.");
 		return;
 	}
 
 	// now we use Prototype's Ajax Updater convenience type to update the
 	// checking signal/control
-	new Ajax.Updater( {
-		success :'checkstress' + linenumber
-	}, 'checkscansion', {
-		parameters : {
-			answer: answer,
-			line :linenumber,
-			poem :$('title').text
-		} , method: 'get' , onComplete: function() {		
-			// here we update the visibility of the "Show Discrepancies control"
-			if ( $$("button.prosody-checkstress span[allowdis]").length == $$("button.prosody-checkstress").length ) {	
-				$("toggle-discrepancies").toggle();
-			}
-		}
-	});
-	
-	
+	new Ajax.Updater(
+			{
+				success :'checkstress' + linenumber
+			},
+			'checkscansion',
+			{
+				parameters : {
+					answer :answer,
+					line :linenumber,
+					poem :$('title').text
+				},
+				method :'get',
+				onComplete : function() {
+					// here we update the visibility of the "Show Discrepancies
+					// control"
+					if ($$("button.prosody-checkstress span[allowdis]").length == $$("button.prosody-checkstress").length) {
+						$("toggle-discrepancies").toggle();
+					}
+					// and here we update any hint buttons
+					updatehintbutton(linenumber);
+				}
+			});
 
 }
 
-function switchfoot(coords){
+function switchfoot(coords) {
 	syllabletext = $(coords).innerHTML;
-	if ( syllabletext.endsWith('|') ) {
-		$(coords).innerHTML = syllabletext.substring(0,syllabletext.length-1);
-	}
-	else {
+	if (syllabletext.endsWith('|')) {
+		$(coords).innerHTML = syllabletext
+				.substring(0, syllabletext.length - 1);
+	} else {
 		$(coords).innerHTML = syllabletext + "|";
 	}
-	
+
 }
 
 function checkfeet(linenumber) {
@@ -103,19 +129,25 @@ function checkfeet(linenumber) {
 
 	// first we assemble the answer from the "stress" members of the appropriate
 	// line
-	var answer = $("prosody:real:" + linenumber).select("span[real]").pluck("textContent").join('');
+	var answer = $("prosody:real:" + linenumber).select("span[real]").pluck(
+			"textContent").join('');
 	/* console.log(answer); */
-	
+
 	// now we use Prototype's Ajax Updater convenience type to update the
 	// checking signal/control
 	new Ajax.Updater( {
 		success :'checkfeet' + linenumber
 	}, 'checkfeet', {
 		parameters : {
-			answer: answer,
+			answer :answer,
 			line :linenumber,
 			poem :$('title').text
-		} , method: 'get'
+		},
+		method :'get',
+		onComplete : function() {	
+			// here we update any hint buttons
+			updatehintbutton(linenumber);
+		}
 	}
 
 	);
@@ -123,54 +155,95 @@ function checkfeet(linenumber) {
 }
 
 // returns an appropriate token element for use as a stress marker
-function marker(real) {	
-	mark = document.createElement("span");
+function marker(real) {
+	mark = document.createElementNS("http://www.w3.org/1999/xhtml","xhtml:span");
 	mark.setAttribute('class', 'prosody-marker');
-	spacer = " ".times(Math.floor(real.textContent.length / 2 ));
-	mark.appendChild(document.createTextNode(spacer + "/" + spacer))	
+	spacer = " ".times(Math.floor(real.textContent.length / 2));
+	mark.appendChild(document.createTextNode(spacer + "/" + spacer))
 	return mark;
 }
 
 // returns an appropriate token element for use as a slack marker
-function slackmarker(real) {	
-	mark = document.createElement("span");
+function slackmarker(real) {
+	mark = document.createElementNS("http://www.w3.org/1999/xhtml","xhtml:span");
 	mark.setAttribute('class', 'prosody-marker');
-	if(real.textContent.length > 2){
-	spacer = " ".times(Math.floor(real.textContent.length / 2 ));
-	mark.appendChild(document.createTextNode(spacer + "∪" + spacer))
+	if (real.textContent.length > 2) {
+		spacer = " ".times(Math.floor(real.textContent.length / 2));
+		mark.appendChild(document.createTextNode(spacer + "∪" + spacer))
 	} else {
-	mark.appendChild(document.createTextNode("∪"))	
+		mark.appendChild(document.createTextNode("∪"))
 	}
 	return mark;
 }
 
 // returns an appropriate placeholder element for use as a blank marker
 function placeholder() {
-	place = document.createElement("span");
+	place = document.createElementNS("http://www.w3.org/1999/xhtml","xhtml:span");
 	place.setAttribute('class', 'prosody-placeholder');
 	return place;
 }
 
-/*
- * function togglestress() { var toggle =
- * document.getElementById('togglestress'); if(toggle.hasClassName('on')) {
- * toggle.removeClassName('on'); toggle.addClassName('off');
- * /*$$('.prosody-marker').each } }
- */
+// changes the visibility of the stress markers
+ function togglestress() {
+	 $$('.prosody-marker').invoke("toggle");
+		
+	}
+
+ 
+
+
 
 // this function highlights those syllables in which real="" and met=""
 // scansions are different.
 // it assumes that any syllable featuring a @real attribute will have also a
 // @met attribute and that
 // they are different.
-
 function toggledifferences(e) {
-	if (e.value == "off"){		
-		document.styleSheets[0].insertRule('span[discrepant] { color: red; }',0);
+	if (e.value == "off") {
+		document.styleSheets[0].insertRule('span[discrepant] { color: red; }',
+				0);
 		e.value = "on";
-	}
-	else if (e.value == "on") {
+	} else if (e.value == "on") {
 		document.styleSheets[0].deleteRule(0);
 		e.value = "off"
 	}
+}
+
+function updatehintbutton(id) {
+	debug("Entering updatehintbutton(" + id + ")");
+	e = $("displaynotebutton" + id);
+	debug("	e = " + $("displaynotebutton" + id) );
+	debug(" e.firstDescendant() = " + $(e).firstDescendant());
+	if ($$("#checkstress" + id + " span[allowdis]").length > 0) {
+		debug("	Passed test.");
+		// if the stress button has an acceptable state
+		//if ($$("#checkfeet" + id + " span[allowdis]").length > 0) {
+			// and the feet button as well, then change visual state
+			e.removeChild($(e).firstDescendant());
+			e.appendChild(clickablehintimage());
+			// and clickability
+			e.setAttribute('onclick', "pophint(this)");
+		//}
+	}
+}
+
+function clickablehintimage() {
+	debug("Entering clickablehintimage()");
+	img = document.createElementNS("http://www.w3.org/1999/xhtml","xhtml:img");
+	img.setAttribute('src', 'images/clickablehint.png');
+	debug("img = ");
+	debug(img);
+	return img;
+}
+
+function pophint(e) {
+	linenumber = e.id.substring(17);
+	hintp = $("hintfor" + linenumber);
+	pop = window.open("","Hint for line " + linenumber, "menubar=no,height=100,width=200");
+	pop.document.body.appendChild( pop.document.importNode(hintp,true));
+}
+
+function debug(s) {
+	if (debugflag) 
+		console.log(s);	
 }
