511faadda5f5a713a6659b1f9960df738d453907
mwm4n <mwm4n@a7c3d585-246a-4d0f-aa80-7a775a0b11e9>
2009-03-20 15:33:28 +0000

    color, pop-up, tei fixes
    git-svn-id: https://subversion.lib.virginia.edu/repos/prosody@112 a7c3d585-246a-4d0f-aa80-7a775a0b11e9

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -283,7 +283,7 @@ function clickablehintimage() {
 function pophint(e) {
     linenumber = e.id.substring(17);
     hintp = $("hintfor" + linenumber);
-    pop = window.open("", "Hint for line " + linenumber, "menubar=no,height=300,width=200");
+    pop = window.open("", "Hint for line " + linenumber,"menubar=no,scrollbars=yes,height=300,width=400");
     pop.document.body.setAttribute("style", "background: #222;color: #fff;font-size: 14px;");
     pop.document.body.appendChild(pop.document.importNode(hintp, true));
 }
