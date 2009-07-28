73bcade17649fa36a2ca9700bb69bc81155c5ff8
wsg4w <wsg4w@a7c3d585-246a-4d0f-aa80-7a775a0b11e9>
2009-07-28 18:27:18 +0000

    git-svn-id: https://subversion.lib.virginia.edu/repos/prosody@326 a7c3d585-246a-4d0f-aa80-7a775a0b11e9

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -314,7 +314,7 @@ function clickablehintimage() {
 function pophint(e) {
 	debug("Entering pophint()");
 	
-	debug("E: " + e.srcElement.classNames());
+	debug("E: " + e.srcElement);
 	
 	
 	// more IE crap
