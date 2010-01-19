56a7fabb0d4421350116ffbb7ee5c05186d77a05
jfg9x <jfg9x@a7c3d585-246a-4d0f-aa80-7a775a0b11e9>
2010-01-19 21:27:42 +0000

    js popup speed
    git-svn-id: https://subversion.lib.virginia.edu/repos/prosody@585 a7c3d585-246a-4d0f-aa80-7a775a0b11e9

--- wp/wp-content/themes/cat4-site/js/popup.js
+++ wp/wp-content/themes/cat4-site/js/popup.js
@@ -3,7 +3,8 @@ jQuery(document).ready(function() {
 		var popup = window.open('','Audio','width=470,height=26');
 		var text = jQuery(this).text();
 		var file = jQuery(this).attr("href");
-		var html = ("<!DOCTYPE html><html lang='en'><head><title>Audio</title></head><body style='margin:20px 20px 0;padding:0;font-size:11px;font-family:arial,sans-serif;background:#666;'><embed id='mAP' width='430' height='26' src='/wp-content/themes/cat4-site/plr.swf' class='adplr' type='application/x-shockwave-flash' flashvars='playerID=mAP&titles=" + text + "&soundFile=" + file + "&autostart=yes' quality='high' menu='false' wmode='transparent' /><a style='color:#fff!important;float:right;' href='" + file + "'>Download</a></body></html>")
+		var html = ("<!DOCTYPE html><html lang='en'><head><title>Audio</title></head><body style='margin:20px 20px 0;padding:0;font-size:11px;font-family:arial,sans-serif;background:#666;'><embed id='mAP' width='430' height='26' src='/wp-content/themes/cat4-site/plr.swf' class='adplr' type='application/x-shockwave-flash' flashvars='playerID=mAP&titles=" + text + "&soundFile=" + file + "&autostart=yes' quality='high' menu='false' wmode='transparent' /><a style='color:#fff!important;float:right;' href='" + file + "'>Download</a></body></html>");
+		popup.document.write(html);
 		/*var player = "<embed id='mAP' width='430' height='26' src='/wp-content/themes/cat4-site/plr.swf' class='adplr' type='application/x-shockwave-flash' flashvars='playerID=mAP&titles=" + text + "&soundFile=" + file + "&autostart=yes' quality='high' menu='false' wmode='transparent' />";
 		popup.document.write("<!DOCTYPE html><html lang='en'><head><title>Audio</title>");
 		popup.document.write("</head><body style='margin:20px 20px 0;padding:0;font-size:11px;font-family:arial,sans-serif;background:#666;'>");
