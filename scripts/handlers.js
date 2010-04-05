d08f4c9175578fbbc8eddb231e541be36c173665
jfg9x <jfg9x@a7c3d585-246a-4d0f-aa80-7a775a0b11e9>
2010-04-05 15:13:03 +0000

    changing image reset behavior for double-digit lines
    git-svn-id: https://subversion.lib.virginia.edu/repos/prosody@647 a7c3d585-246a-4d0f-aa80-7a775a0b11e9

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -124,7 +124,7 @@ function switchstress(shadowspan) {
         },
         150);
     }
-	var stressimg=$("checkstress" + shadowspan.id.substring(15,16)).firstDescendant().firstDescendant();
+	var stressimg=$("checkstress" + shadowspan.id.substring(15)).firstDescendant().firstDescendant();
 	if(stressimg && stressimg.readAttribute("src")!="images/stress-default.png"){
 		stressimg.writeAttribute("src","images/stress-default.png");
 	}
